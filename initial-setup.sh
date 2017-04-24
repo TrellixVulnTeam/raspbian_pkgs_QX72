7a63b2794460d8ba8b2398c0ed069a65916d0855
Kevin <walchko@users.noreply.github.com>
2017-04-23 20:14:02 -0600

    Update initial-setup.sh

--- initial-setup.sh
+++ initial-setup.sh
@@ -6,12 +6,18 @@ if [ "$EUID" -ne 0 ]
   exit
 fi
 
+echo ""
+echo "============================="
+echo "| Let's update some pkgs    |"
+echo "============================="
+echo ""
+
 # update
 apt-get update
 apt-get -y upgrade
 
 # programming
-apt-get -y install cmake pkg-config build-essential git libpcap
+apt-get -y install cmake pkg-config build-essential git libpcap0.8-dev
 
 # admin
 apt-get -y install nmap htop samba samba-common-bin arp-scan wget curl
@@ -25,9 +31,45 @@ apt-get -y install jp2a
 # install node.js
 curl -sL https://deb.nodesource.com/setup_7.x | sudo -E bash -
 apt-get install -y nodejs
+npm install -g httpserver
+
+echo ""
+echo "============================="
+echo "| Let's grab some repos     |"
+echo "============================="
+echo ""
+
+# packagecloud.io
+cd /tmp
+curl -s https://packagecloud.io/install/repositories/walchko/robots/script.deb.sh | sudo bash
+apt-get update
 
 # install some repos
-cd ~/github && git clone git@github.com:walchko/dotfiles.git
+su pi -c "cd /home/pi/github && git clone git@github.com:walchko/dotfiles.git"
+
+echo ""
+echo "============================="
+echo "| Go python                 |"
+echo "============================="
+echo ""
+
+apt-get -y install python-dev
+cd /tmp && wget https://bootstrap.pypa.io/get-pip.py && python get-pip.py
+pip install -U pip setuptools wheel
+
+apt-get -y install python3k
+pip3 install -U pip setuptools wheel
+
+echo ""
+echo "============================="
+echo "| Clean up and fix perms    |"
+echo "============================="
+echo ""
+
+chown -R pi:pi /home/pi
+chown -R pi:pi /usr/local
+chown -R pi:pi /usr/lib/python2.7/dist-packages/
+chown -R pi:pi /usr/lib/node_modules/
 
 echo ""
 echo "============================="
