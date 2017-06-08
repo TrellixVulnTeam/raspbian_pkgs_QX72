b99fb86a90207f361efb9b572cb487bc5dd3755b
Kevin <walchko@users.noreply.github.com>
2017-06-07 22:40:36 -0600

    Create build-python3.sh

--- python3/build-python3.sh
+++ python3/build-python3.sh
@@ -13,15 +13,16 @@ echo "Installing to: $PYTHON_INSTALL_DIR"
 echo "-------------------------------------------------"
 echo ""
 
+. ./update-python3.sh
 
-if [[ "$OSTYPE" == "linux-gnu" ]] || [[ "$OSTYPE" == "linux-gnueabihf" ]]; then
-	# apt-get update
-	apt-get -y upgrade
-	apt-get -y install build-essential cmake git pkg-config
-	apt-get -y install tk-dev libncurses5-dev libncursesw5-dev libreadline6-dev libdb5.3-dev 
-	apt-get -y install libgdbm-dev libsqlite3-dev libssl-dev libbz2-dev libexpat1-dev 
-	apt-get -y install liblzma-dev zlib1g-dev mime-support
-fi
+#if [[ "$OSTYPE" == "linux-gnu" ]] || [[ "$OSTYPE" == "linux-gnueabihf" ]]; then
+#	# apt-get update
+#	apt-get -y upgrade
+#	apt-get -y install build-essential cmake git pkg-config
+#	apt-get -y install tk-dev libncurses5-dev libncursesw5-dev libreadline6-dev libdb5.3-dev 
+#	apt-get -y install libgdbm-dev libsqlite3-dev libssl-dev libbz2-dev libexpat1-dev 
+#	apt-get -y install liblzma-dev zlib1g-dev mime-support
+#fi
 
 if [ ! -f Python-$PYTHON_VERSION.tar.xz ]; then
 	wget https://www.python.org/ftp/python/3.6.1/Python-$PYTHON_VERSION.tar.xz
