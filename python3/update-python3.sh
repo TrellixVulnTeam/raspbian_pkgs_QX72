ede64ac3a1301d416f19dcb327009a2f2cb73ca0
Kevin <walchko@users.noreply.github.com>
2017-06-07 22:39:24 -0600

    Create update-python3.sh

---
+++ python3/update-python3.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+set -e
+
+# check if we are root
+if [ "$EUID" -ne 0 ]; then
+  echo "Please run as root"
+  exit 1
+fi
+
+if [[ "$OSTYPE" == "linux-gnu" ]] || [[ "$OSTYPE" == "linux-gnueabihf" ]]; then
+	# apt-get update
+	apt-get -y upgrade
+	apt-get -y install build-essential cmake git pkg-config
+	apt-get -y install tk-dev libncurses5-dev libncursesw5-dev libreadline6-dev libdb5.3-dev 
+	apt-get -y install libgdbm-dev libsqlite3-dev libssl-dev libbz2-dev libexpat1-dev 
+	apt-get -y install liblzma-dev zlib1g-dev mime-support
+  
+  echo ""
+  echo "Done!"
+  echo ""
+  
+else
+  echo ""
+  echo "Sorry, don't support our OS"
+  echo ""
+fi
