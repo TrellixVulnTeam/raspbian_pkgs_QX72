3dd92f3de5795f6165ece748bece618a4ca4da03
walchko <walchko@users.noreply.github.com>
2017-06-29 22:48:24 +0000

    updates

--- python3/update-python3.sh
+++ python3/update-python3.sh
@@ -12,14 +12,15 @@ if [[ "$OSTYPE" == "linux-gnu" ]] || [[ "$OSTYPE" == "linux-gnueabihf" ]]; then
 	# apt-get update
 	apt-get -y upgrade
 	apt-get -y install build-essential cmake git pkg-config
-	apt-get -y install tk-dev libncurses5-dev libncursesw5-dev libreadline6-dev libdb5.3-dev 
-	apt-get -y install libgdbm-dev libsqlite3-dev libssl-dev libbz2-dev libexpat1-dev 
+	apt-get -y install tk-dev libncurses5-dev libncursesw5-dev libreadline6-dev libdb5.3-dev
+	apt-get -y install libgdbm-dev libsqlite3-dev libssl-dev libbz2-dev libexpat1-dev
 	apt-get -y install liblzma-dev zlib1g-dev mime-support
-  
+	apt-get -y install libffi-dev
+
   echo ""
   echo "Done!"
   echo ""
-  
+
 else
   echo ""
   echo "Sorry, don't support our OS"
